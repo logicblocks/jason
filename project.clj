5fa302ec7a3ee5339354ea894a1b2b4f97d6f7c5
Circle CI <circleci@logicblocks.io>
2023-12-09 13:36:58 +0000

    Pre-release version 1.0.1-RC3 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/jason "1.0.1-RC2"
+(defproject io.logicblocks/jason "1.0.1-RC3"
   :description "Factory functions around jsonista, mostly for key conversion."
   :url "https://github.com/logicblocks/jason"
 
